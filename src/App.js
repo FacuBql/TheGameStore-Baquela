cdb8213593afbe521f7a2ffddfca50d2575010d4
FacuBql <facubaquela@gmail.com>
2022-04-20 15:26:01 -0300

    correccion desafio NavBar

--- src/App.js
+++ src/App.js
@@ -1,15 +1,8 @@
+import NavBar from "./components/NavBar"
+
 const App = () => {
   return (
-    <header className="header">
-      <h1 className="header_titulo">The Game Store</h1>
-      <nav className="nav">
-        <a href="#">Estrenos</a>
-
-        <a href="#">Los más pedidos</a>
-
-        <a href="#">Retro</a>
-      </nav>
-    </header>
+  <NavBar></NavBar>
   );
 };
 export default App;
