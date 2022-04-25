36064d31cc0c986cd22168fef769d73fb4c9d87e
FacuBql <facubaquela@gmail.com>
2022-04-25 16:04:10 -0300

    desafio contador

---
+++ src/components/ItemCount.js
@@ -0,0 +1,47 @@
+import { useState } from "react";
+
+const ItemCount = ({ init, stock, onAdd }) => {
+  const [counter, setCounter] = useState(init);
+  const [success, setSuccess] = useState(false);
+
+  const add = () => {
+    setCounter(counter + 1);
+  };
+
+  const remove = () => {
+    setCounter(counter - 1);
+  };
+
+  const confirm = () => {
+    //onAdd(counter);
+    if (counter <= stock) {
+      setSuccess(true);
+    } else if (counter < stock) {
+      setSuccess(false);
+    }
+  };
+
+  var stock = 10;
+
+  if (success) {
+    return (
+      <div>
+        <p>Unidades agregadas: {counter}</p>
+        <button onClick={remove}>-</button>
+        <button onClick={confirm}>Agregar</button>
+        <button onClick={add}>+</button>
+        <p>Agregado al carrito exitosamente.</p>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <p>Unidades agregadas: {counter}</p>
+        <button onClick={remove}>-</button>
+        <button onClick={confirm}>Agregar</button>
+        <button onClick={add}>+</button>
+      </div>
+    );
+  }
+};
+export default ItemCount;
