f940349554e1d462ffb97cc47967c550b030f219
FacuBql <facubaquela@gmail.com>
2022-04-25 15:54:32 -0300

    Desafio contador

--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -1,7 +1,9 @@
+import ItemCount from "./ItemCount";
 const ItemListContainer = (props) => {
   return (
     <div className="greeting">
       <p>{props.greeting}</p>
+      <ItemCount stock={10} init={0} onAdd={() => {}} />
     </div>
   );
 };
