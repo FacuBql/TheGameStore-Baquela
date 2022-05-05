b87fbe08f637659237092d55e3e4071a2a96db33
FacuBql <facubaquela@gmail.com>
2022-05-04 21:42:07 -0300

    correcion 2 desafio itemdetail

--- src/components/ItemDetail.js
+++ src/components/ItemDetail.js
@@ -1,9 +1,7 @@
 const ItemDetail = ({ products }) => {
   return (
     <section>
-      {products.map((products) => {
-        return <ItemDetail key={products.id} products={products} />;
-      })}
+    <ItemDetail/>
     </section>
   );
 };
